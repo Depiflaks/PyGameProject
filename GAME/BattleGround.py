1b32823f3604f54fcfb2783f6d198e523979cb2b
Depiflaks <ssmirnov130405@gmail.com>
2023-01-11 02:34:31 +0300

    движение экрана с персонажем

--- GAME/BattleGround.py
+++ GAME/BattleGround.py
@@ -37,44 +37,52 @@ class Board(pygame.sprite.LayeredUpdates):
     def updateToRedPoint(self, point):
         move_x, move_y = CENTER[0] - point[0], CENTER[1] - point[1]
         for i in self.sprites():
-            i.rect.x += move_x
-            i.rect.y += move_y
+            i.x += move_x
+            i.y += move_y
+            i.rect.x = i.x
+            i.rect.y = i.y
+
 
 
 class Cell(pygame.sprite.Sprite):
     def __init__(self, board, ID, x, y):
         super().__init__(board)
-        self.forms = [ID]
+        self.frames = []
         con = sqlite3.connect("../resources/id.db")
         try:
             result = list(con.cursor().execute(f"""SELECT "Активная форма", "Тип" FROM ObjectID
                 WHERE ID = {ID} """).fetchall()[0])
         except:
-            result = [None, 4]
+            result = [1, 4]
         self.type = result[1]
-        if result[0]:
-            self.forms.append(result[0])
         self.ID = ID
         if self.ID == 0:
             self.image = pygame.Surface((CELL_SIZE[0], CELL_SIZE[1]), pygame.SRCALPHA, 32)
             self.rect = pygame.Rect(x, y, CELL_SIZE[0], CELL_SIZE[1])
+            self.x = x
+            self.y = y
             pygame.draw.rect(self.image, BACKGROUND_COLOR, (0, 0, CELL_SIZE[0], CELL_SIZE[1]))
             board.change_layer(self, 0)
-        elif self.ID == 10 or self.ID == 11:
+        elif self.ID == 10 or self.ID == 13:
             Cell(board, 1, x, y)
             self.image = load_image(f'cells/{self.ID}.png')
             self.image = pygame.transform.scale(self.image, DOOR_SIZE)
             self.rect = self.image.get_rect()
-            self.rect.x = x
-            self.rect.y = y - DOOR_SIZE[1] + CELL_SIZE[1]
-            board.change_layer(self, 3)
+            self.x = x
+            self.y = y - DOOR_SIZE[1] + CELL_SIZE[1]
+            board.change_layer(self, 5)
         else:
             self.image = load_image(f'cells/{self.ID}.jpg')
             self.image = pygame.transform.scale(self.image, CELL_SIZE)
             self.rect = self.image.get_rect()
-            self.rect.x = x
-            self.rect.y = y
+            self.x = x
+            self.y = y
             board.change_layer(self, 1)
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+    def cutFrames(self, sheet, count):
+        pass
 
 
 class Wall(pygame.sprite.Sprite):
@@ -83,10 +91,13 @@ class Wall(pygame.sprite.Sprite):
         self.image = load_image(f'cells/21.png')
         self.image = pygame.transform.scale(self.image, WALL_SIZE)
         self.rect = self.image.get_rect()
-        self.rect.x = x
+        self.x = x
+        self.y = y - WALL_SIZE[1] + 90 + CELL_SIZE[1]
+        self.rect.x = self.x
         self.type = 4
-        self.rect.y = y - WALL_SIZE[1] + 90 + CELL_SIZE[1]
+        self.rect.y = self.y
         board.change_layer(self, 2)
+
 
 
 if __name__ == '__main__':
