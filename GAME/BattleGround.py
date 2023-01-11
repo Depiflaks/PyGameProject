9d23a0a5c03fdc6ae5e309482b2ac644e0d4bcb3
Depiflaks <ssmirnov130405@gmail.com>
2023-01-11 03:18:29 +0300

    анимация объектов

--- GAME/BattleGround.py
+++ GAME/BattleGround.py
@@ -55,34 +55,30 @@ class Cell(pygame.sprite.Sprite):
         except:
             result = [1, 4]
         self.type = result[1]
+        self.frames_count = result[0]
         self.ID = ID
-        if self.ID == 0:
-            self.image = pygame.Surface((CELL_SIZE[0], CELL_SIZE[1]), pygame.SRCALPHA, 32)
-            self.rect = pygame.Rect(x, y, CELL_SIZE[0], CELL_SIZE[1])
-            self.x = x
-            self.y = y
-            pygame.draw.rect(self.image, BACKGROUND_COLOR, (0, 0, CELL_SIZE[0], CELL_SIZE[1]))
-            board.change_layer(self, 0)
-        elif self.ID == 10 or self.ID == 13:
+        self.x = x
+        self.y = y
+        if self.type == 3:
             Cell(board, 1, x, y)
-            self.image = load_image(f'cells/{self.ID}.png')
-            self.image = pygame.transform.scale(self.image, DOOR_SIZE)
-            self.rect = self.image.get_rect()
-            self.x = x
-            self.y = y - DOOR_SIZE[1] + CELL_SIZE[1]
+            self.cutFrames(load_image(f'cells/{self.ID}.png'), self.frames_count)
+            self.y -= DOOR_SIZE[1] - CELL_SIZE[1]
             board.change_layer(self, 5)
         else:
-            self.image = load_image(f'cells/{self.ID}.jpg')
-            self.image = pygame.transform.scale(self.image, CELL_SIZE)
-            self.rect = self.image.get_rect()
-            self.x = x
-            self.y = y
-            board.change_layer(self, 1)
+            self.cutFrames(load_image(f'cells/{self.ID}.jpg'), self.frames_count)
+            board.change_layer(self, 0 if self.ID == 0 else 1)
+        self.image = self.frames[0]
         self.rect.x = self.x
         self.rect.y = self.y
 
     def cutFrames(self, sheet, count):
-        pass
+        w = DOOR_SIZE[1] if self.type == 3 else CELL_SIZE[1]
+        self.rect = pygame.Rect(0, 0, CELL_SIZE[0], w)
+        sheet = pygame.transform.scale(sheet, (CELL_SIZE[0] * count, w))
+        for i in range(count):
+            frame_location = (self.rect.w * i, 0)
+            #print(frame_location, sheet.get_width(), sheet.get_height(), self.rect.size)
+            self.frames.append(sheet.subsurface(pygame.Rect(frame_location, self.rect.size)))
 
 
 class Wall(pygame.sprite.Sprite):
@@ -111,7 +107,7 @@ if __name__ == '__main__':
 
     screen.fill(BACKGROUND_COLOR)
 
-    board = Board('l1/l1.csv')
+    board = Board('l2/l2.csv')
     #board.updateToRedPoint((300, 300))
     board.draw(screen)
 
