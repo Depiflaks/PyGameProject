bdd5e4128eba803594704e7fcb24a8a9f28429ce
spiritonchic <vanya.ershov.05@mail.ru>
2023-01-20 20:51:02 +0300

    всё

--- GAME/Main.py
+++ GAME/Main.py
@@ -8,28 +8,31 @@ screen.fill((10, 255, 10))
 ind = False
 # Основной игровой цикл
 while running:
-    # Обработка всех событий
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT or ind:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
+    try:
+        # Обработка всех событий
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or ind:
                 running = False
-            if event.key == pygame.K_c:
-                pygame.display.iconify()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    running = False
+                if event.key == pygame.K_c:
+                    pygame.display.iconify()
+            if menu.indicator:
+                ind = menu.update_forms(event)
+            elif end.indicator:
+                end.update_forms(event)
+            else:
+                levelManager.level.updateStates(event)
         if menu.indicator:
-            ind = menu.update_forms(event)
+            menu.draw(screen)
         elif end.indicator:
-            end.update_forms(event)
+            end.draw(screen)
         else:
-            levelManager.level.updateStates(event)
-    if menu.indicator:
-        menu.draw(screen)
-    elif end.indicator:
-        end.draw(screen)
-    else:
-        levelManager.next(levelManager.level.update())
-    pygame.display.flip()
-    clock.tick(FPS)
+            levelManager.next(levelManager.level.update())
+        pygame.display.flip()
+        clock.tick(FPS)
+    except Exception:
+        pass
 pygame.quit()
 sys.exit()
